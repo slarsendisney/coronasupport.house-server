ae8b562c2f6b75f4dacd05edb9b29963025ab575
slarsendisney <s.larsendisney@gmail.com>
2020-03-23 22:14:33 +0000

    now uses firebase numbers

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ if (process.env.NODE_ENV !== "production") {
     path: `.env`
   });
 }
+
+const firebase = require("firebase");
+
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID
+});
+
+var db = firebase.firestore();
+let phoneNumbers = [];
+db.collection("volunteers").onSnapshot(subCollectionSnapshot => {
+  let numbers = [];
+  subCollectionSnapshot.forEach(subDoc => {
+    if (subDoc.data().phone_number) {
+      numbers.push(subDoc.data().phone_number);
+    }
+  });
+  phoneNumbers = numbers;
+});
 const accountSid = process.env.ACCOUNTSID;
 const authToken = process.env.AUTHTOKEN;
 
@@ -33,13 +55,14 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.post("/send", (req, res) => {
   const name = req.body.name;
   const message = `Hi Volunteer! We've recieved a new request from ${name}. Head to https://coronasupport.house/volunteers to claim it.`;
-  client.messages
-    .create({
+  phoneNumbers.forEach(number => {
+    client.messages.create({
       body: message,
       from: "+441173253685",
-      to: "+447926147958"
-    })
-    .then(() => res.send("OK"));
+      to: number
+    });
+  });
+  res.send("OK");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
